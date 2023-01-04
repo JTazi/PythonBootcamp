c797c9661c4b05f14c312d20d18c602ebb921793
JTazi <99365644+JTazi@users.noreply.github.com>
2023-01-04 10:48:58 -0600

    Add files via upload

---
+++ bracket_balance.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  4 10:25:03 2023
+Problem: 
+    take string input of parentheses, square and curly brackets
+    check if left and right brackets are equal and in the right order
+    return True/False
+"""
+test1 = '({[]})'
+test2 = '({[})]'
+
+
+def bracket_balance(brackets):
+    #create list to store left brackets in order
+    left_stack = []
+    #lists to identify lefts and rights
+    lefts = ['(','{','[']
+    rights = [')','}',']']
+    #dictionary for storing bracket pairs
+    brack_dict = {')':'(', ']':'[', '}':'{'}
+    #iterate through input string and store left brackets in left_stack
+    #once a right is encountered, check if it pairs with top of left_stack
+    #if it matches, pop top of stack and continue
+    for b in brackets:
+        if b in lefts:
+            left_stack.append(b)
+        if b in rights:
+            if left_stack[-1] == brack_dict[b]:
+                left_stack.pop()
+            else:
+                return False
+    return True
+            
+
+print(bracket_balance(test1))
+print(bracket_balance(test2))
